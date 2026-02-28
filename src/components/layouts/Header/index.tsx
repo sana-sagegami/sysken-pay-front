61a9442e4ace1e233c383e3350d4005088435320
kkkk0113 <koto1658@gmail.com>
2026-03-01 03:01:03 +0900

    アイコンをimgからiconへ移動

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -14,13 +14,13 @@ function Header({ title, right = "none", shadow = false }: HeaderProps) {
       style={{ boxShadow: shadow ? "0 1.8px 9px 0 #17223330" : "none" }}
     >
       <div className={styles.left}>
-        <img src="/img/syskenicon.png" alt="Icon" className={styles.icon} />
+        <img src="/icon/syskenicon.png" alt="Icon" className={styles.icon} />
         <h1 className={styles.title}>{title}</h1>
       </div>
       {right === "setting" && (
         <Link to="/admin">
           <img
-            src="/img/setting.png"
+            src="/icon/setting.png"
             alt="Setting"
             className={styles.setting}
           />
