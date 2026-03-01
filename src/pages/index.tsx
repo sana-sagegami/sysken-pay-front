d844343103ca90c101b8e307b1e2906e904bab4c
kkkk0113 <koto1658@gmail.com>
2026-03-01 14:22:49 +0900

    top画面の修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
-import PayMethodButton from "../components/feature/buy/PayMethodButton";
-import Header from "../components/layouts/Header";
 import { Input } from "../components/ui/Input";
 
 export default function Home() {
   return (
     <div>
-      <Header title="決済方法" right="toTop" />
-      {/* <h1>ホーム</h1>
+      <h1>ホーム</h1>
       <p>Sysken Pay システム</p>
-			<Input /> */}
-      <PayMethodButton type="cash" />
-      <PayMethodButton type="syspay" />
+      <Input />
     </div>
   );
 }
