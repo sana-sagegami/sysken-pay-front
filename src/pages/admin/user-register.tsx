0b9ee19852a7e75d18e713296633b486a676e6bc
sana-sagegami <sana01237@icloud.com>
2026-03-20 20:27:22 +0900

    chore: ユーザー登録画面の追加

--- src/pages/admin/user-register.tsx
+++ src/pages/admin/user-register.tsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+import { BarcodeReader } from "../../components/ui/BarcodeReader";
+import { useNavigate } from "react-router-dom";
+import Header from "../../components/layouts/Header";
+import { ArrowButton } from "../../components/ui/Button";
+import { useUserStore } from "../../store/useUserStore";
+
 export default function UserRegisterPage() {
-  return <div>ユーザー登録</div>;
+  const [mode] = useState<"product" | "member">("member");
+  const navigate = useNavigate();
+	const setScannedUser = useUserStore((state) => state.setScannedUser);
+	
+
+  const handleScan = (barcode: string) => {
+    // TODO: APIからUser情報を取得
+    setScannedUser({
+      userId: barcode,
+      userName: "さな",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    navigate("/buy/syspay/confirm");
+  };
+
+  return (
+    <div className="flex flex-col h-screen overflow-hidden">
+      <Header title="商品購入" />
+      <div className="flex flex-1 items-center justify-center">
+        <BarcodeReader
+          mode={mode}
+          onScan={handleScan}
+          placeholder="学生証のバーコードをかざしてください"
+        />
+      </div>
+      <ArrowButton type="back" />
+    </div>
+  );
 }
